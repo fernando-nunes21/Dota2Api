fbd204f2d8270619dd34559c823ad3a316e820ce
fernando-nunes21 <luis@ubots.com.br>
2021-08-24 10:00:25 -0300

    Realizado os ajustes comentados no code review. Controllers individuais para cada domínio.

---
+++ api/src/main/groovy/com/dota/api/Skins/SkinsController.groovy
@@ -0,0 +1,36 @@
+package com.dota.api.Skins
+
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.DeleteMapping
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.PutMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/v1/skins")
+class SkinsController {
+
+    @GetMapping
+    ResponseEntity getSkins() {
+
+    }
+
+
+    @PostMapping
+    ResponseEntity createSkin() {
+
+    }
+
+    @PutMapping("/{id}")
+    ResponseEntity editSkin(@PathVariable Integer id) {
+
+    }
+
+    @DeleteMapping("/{id}")
+    ResponseEntity deleteSkin(@PathVariable Integer id) {
+
+    }
+}
