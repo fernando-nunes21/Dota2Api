f0fe1ead6aad6f6fe0dc18730dc4b63bfbdb87be
fernando-nunes21 <luis@ubots.com.br>
2021-08-25 11:31:26 -0300

    Consertado teste anterior, que não estava fazendo construido corretamente e não fazia o que eu tinha moldado. Agora o teste passa automaticamente, pois 200 e o padrão caso nao haja problemas. Esta sendo adicionado o teste para not found, que nao funciona por falta de implementação.

--- api/src/test/groovy/com/dota/api/HeroesControllerTests.groovy
+++ api/src/test/groovy/com/dota/api/HeroesControllerTests.groovy
@@ -2,6 +2,7 @@ package com.dota.api
 
 import com.dota.api.Heroes.HeroesController
 import com.fasterxml.jackson.databind.ObjectMapper
+import org.junit.jupiter.api.BeforeEach
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
 import org.springframework.http.ResponseEntity
 import org.springframework.test.web.servlet.MockMvc
@@ -19,25 +20,27 @@ class HeroesControllerTests extends Specification {
 
     private HeroesController heroesController = new HeroesController()
     private MockMvc mockMvc = MockMvcBuilders.standaloneSetup(heroesController).build()
-    private ObjectMapper objectMapper
 
-    HeroesControllerTests(ObjectMapper objectMapper){
-        this.objectMapper = objectMapper
-    }
-
-    def "Get Heroes should return a list of heroes and status code 200"(){
+    def "Get Heroes should return a status code 200 when everything is normal"(){
         given:
 
         when:
-        ResultActions resultActions = mockMvc.perform(get("/v1/heroes"))
-        MvcResult contentResult = resultActions.andReturn()
-        String response = contentResult.getResponse().getContentAsString()
-        List<Heroes> heroes = objectMapper.readValue(response,List<Heroes>.class)
+        ResultActions response = mockMvc.perform(get("/v1/heroes"))
 
         then:
-        resultActions.andExpect(status().isOk())
-        heroes.size() > 0
+        response.andExpect(status().isOk())
 
+    }
+
+    def "Get Heroes should return a status code 404 and throw exception when not found any hero"(){
+        given:
+
+        when:
+        ResultActions response = mockMvc.perform(get("/v1/heroes"))
+
+        then:
+        response.andExpect(status().isNotFound())
+        thrown(NotFoundAnyHero)
     }
 
 }
