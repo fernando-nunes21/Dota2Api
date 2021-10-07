6b24136fc97eda11c36817a0b6b64ab31bf62810
fernando-nunes21 <luis@ubots.com.br>
2021-10-07 10:01:07 -0300

    Fix: Removendo desnecesários

--- api/src/main/groovy/com/dota/api/ApiApplication.groovy
+++ api/src/main/groovy/com/dota/api/ApiApplication.groovy
@@ -1,15 +1,11 @@
 package com.dota.api
 
-import com.dota.api.Handler.RestExceptionHandler
-import com.dota.api.Heroes.HeroService
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.context.annotation.Import
 import org.springframework.jdbc.core.JdbcTemplate
 
 @SpringBootApplication
-@Import(RestExceptionHandler)
 class ApiApplication {
 
 	@Autowired
